771dd7937d21e5a23c832a32a6b19366247a4e03
Adrien Merignac <adrien.merignac92@gmail.com>
2018-05-08 15:37:19 +0200

    Revert "Commit of Nba New Application"
    This reverts commit 15dfc1b2e183e5e8d6b87b6c4d3742c8cdf99933.

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,4 +1,3 @@
-import { AllPlayers } from './../models';
 import { Team } from '../models';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -13,7 +12,6 @@ import { NbaService } from './../nba.service';
 export class TeamComponent implements OnInit {
 
   team: Team;
-  players: AllPlayers;
 
   constructor(private route: ActivatedRoute, private nbaService: NbaService, private router: Router) {
     this.route.params.subscribe(res => {
@@ -22,11 +20,6 @@ export class TeamComponent implements OnInit {
       this.nbaService.getTeams(2017).subscribe(results => {
         console.log('team', results);
         this.team = results;
-      });
-
-      this.nbaService.getTeamRoster(2017).subscribe(results => {
-        console.log('players', results);
-        this.players = results;
       });
     });
   }
